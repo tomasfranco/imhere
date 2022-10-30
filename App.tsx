7a609c354ab35d667926612b4a40d533079d45f4
Tomas Franco <tomasfranco@Tomas-MacBook-Pro.local>
2022-10-30 07:58:41 -0300

    Utilizando StyleSheet para estilizar - StyleSheet for styling

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet } from "react-native"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+export default function App(){
+return (
+  <View style={styles.container}>
+  <Text style={styles.eventName}>Nome do evento</Text>
+
+  <Text style={styles.eventDate}>Sexta, 4 de novembro de 2022</Text>
+  </View>
+)}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#131016',
+    padding: 24
   },
-});
+  eventName: {
+    color: '#FDFCFE',
+    fontSize: 12,
+    fontWeight: 'bold',
+    marginTop: 48
+  },
+  eventDate: {
+    color: '#6B6B6b',
+    fontSize: 16
+  }
+})
